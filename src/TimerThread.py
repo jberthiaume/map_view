6d440ad0e87c88a56fb9a612737441dcab17062d
jberthiaume <jberth.pwc@gmail.com>
2013-07-23 16:28:07 +0100

    Re-added TimerThread class

---
+++ src/TimerThread.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import time
+from threading import Thread
+
+class TimerThread(Thread):
+    def __init__(self, parent, interval):
+        self.stopped = False
+        self.parent = parent
+        self.interval = interval
+        Thread.__init__(self)
+
+    def run(self):
+        while not self.stopped:
+            time.sleep(self.interval)
+            self.parent.ok = True
+            print "Set OK True"
+
+    def SetInterval(self, interval):
+        self.interval = interval
+
+                
